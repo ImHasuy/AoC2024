1d502ddfc0182d52f5454b84f28e92fa5f70f007
Gáspár Csanád Tamás <92604144+ImHasuy@users.noreply.github.com>
2024-12-07 01:41:37 +0100

    Add files via upload

---
+++ AdventOfCode1/AdventOfCode1/DataObj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode1
+{
+    internal class DataObj
+    {
+        public int LeftInt { get; set; }
+        public int RightInt { get; set; }
+
+        public int DiffBetTwo{ get { return Math.Abs(LeftInt - RightInt); }}
+
+        public DataObj(int leftInt, int rightInt)
+        {
+            LeftInt = leftInt;
+            RightInt = rightInt;
+        }
+    }
+}
---
+++ AdventOfCode1/AdventOfCode1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AdventOfCode1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DataObj> list = new List<DataObj>();
+            string[] lista1 = File.ReadAllLines("input.txt");
+
+            string[] LeftSide = lista1.Select(x => x.Split("   ")[0]).Order().ToArray();
+            string[] RightSide = lista1.Select(x => x.Split("   ")[1]).Order().ToArray();
+
+            for (int i = 0; i < lista1.Length; i++)
+            {
+                list.Add(new DataObj(int.Parse(LeftSide[i]), int.Parse(RightSide[i])));
+            }
+                
+
+            Console.WriteLine(list.Sum(x=>x.DiffBetTwo));
+
+            Console.WriteLine("Masodik feladat");
+
+            int temp_counter =0;
+
+            for (int i = 0; i < LeftSide.Length; i++)
+            {
+                temp_counter += int.Parse(LeftSide[i]) * RightSide.Count(m => m == LeftSide[i]);
+            }
+
+            Console.WriteLine(temp_counter);
+        }
+    }
+}
